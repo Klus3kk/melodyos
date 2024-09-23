056804f2437bd3058109135a4ddc440fa5be7a12
Klus3kk <lukasz.bielaszewski@onet.pl>
2024-09-23 23:22:06 +0200

    Part 1 finished.

--- melodyos/src/main.rs
+++ melodyos/src/main.rs
@@ -1,12 +1,16 @@
-#![no_std]
+#![no_std] // Don'y link the Rust standard library
+#![no_main] // Because we don't want to use main
 
 // Panic
 use core::panic::PanicInfo;
+
+#[no_mangle] // Disabling name mangling
+pub extern "C" fn _start() -> ! { // Entry point
+    loop{}
+}
 
 // Function called on panic
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! { // ! - never
     loop{}
 }
-
-fn main() {}
---
+++ melodyos/target/thumbv7em-none-eabihf/debug/deps/melodyos-013f1b639a048bb0.d
@@ -0,0 +1,5 @@
+/home/cluesec/Codes/melodyos/melodyos/target/thumbv7em-none-eabihf/debug/deps/melodyos-013f1b639a048bb0: src/main.rs
+
+/home/cluesec/Codes/melodyos/melodyos/target/thumbv7em-none-eabihf/debug/deps/melodyos-013f1b639a048bb0.d: src/main.rs
+
+src/main.rs:
