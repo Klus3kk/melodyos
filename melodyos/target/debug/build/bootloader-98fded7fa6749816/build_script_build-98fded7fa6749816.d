343da4d2f90b457cd3b053d45564a8fd7117d692
Klus3kk <lukasz.bielaszewski@onet.pl>
2024-09-28 17:13:10 +0200

    Bugfix the bootloader creation.

---
+++ melodyos/target/debug/build/bootloader-98fded7fa6749816/build_script_build-98fded7fa6749816.d
@@ -0,0 +1,5 @@
+/home/cluesec/Codes/melodyos/melodyos/target/debug/build/bootloader-98fded7fa6749816/build_script_build-98fded7fa6749816: /home/cluesec/.cargo/registry/src/index.crates.io-6f17d22bba15001f/bootloader-0.9.29/build.rs
+
+/home/cluesec/Codes/melodyos/melodyos/target/debug/build/bootloader-98fded7fa6749816/build_script_build-98fded7fa6749816.d: /home/cluesec/.cargo/registry/src/index.crates.io-6f17d22bba15001f/bootloader-0.9.29/build.rs
+
+/home/cluesec/.cargo/registry/src/index.crates.io-6f17d22bba15001f/bootloader-0.9.29/build.rs:
