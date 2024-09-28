3a9049311d7e0ecabc9d55881b04504ec3daa472
Klus3kk <lukasz.bielaszewski@onet.pl>
2024-09-28 17:13:30 +0200

    Bugfix the bootloader creation.

--- melodyos/target/debug/build/compiler_builtins-b56b888c0a4d829d/build_script_build-b56b888c0a4d829d.d
+++
@@ -1,6 +0,0 @@
-/home/cluesec/Codes/melodyos/melodyos/target/debug/build/compiler_builtins-b56b888c0a4d829d/build_script_build-b56b888c0a4d829d: /home/cluesec/.cargo/registry/src/index.crates.io-6f17d22bba15001f/compiler_builtins-0.1.126/build.rs /home/cluesec/.cargo/registry/src/index.crates.io-6f17d22bba15001f/compiler_builtins-0.1.126/configure.rs
-
-/home/cluesec/Codes/melodyos/melodyos/target/debug/build/compiler_builtins-b56b888c0a4d829d/build_script_build-b56b888c0a4d829d.d: /home/cluesec/.cargo/registry/src/index.crates.io-6f17d22bba15001f/compiler_builtins-0.1.126/build.rs /home/cluesec/.cargo/registry/src/index.crates.io-6f17d22bba15001f/compiler_builtins-0.1.126/configure.rs
-
-/home/cluesec/.cargo/registry/src/index.crates.io-6f17d22bba15001f/compiler_builtins-0.1.126/build.rs:
-/home/cluesec/.cargo/registry/src/index.crates.io-6f17d22bba15001f/compiler_builtins-0.1.126/configure.rs:
