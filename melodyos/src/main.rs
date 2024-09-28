384db472e3e0bebec7b8958d6679a8b108da3ae2
Klus3kk <lukasz.bielaszewski@onet.pl>
2024-09-28 17:06:38 +0200

    Printing to screen implemented in main.rs, updated config.toml for only writing cargo build (without the --target).

--- melodyos/src/main.rs
+++ melodyos/src/main.rs
@@ -10,7 +10,18 @@ fn panic(_info: &PanicInfo) -> ! { // ! - never
     loop{}
 }
 
+static HELLO: &[u8] = b"Hello World!";
+
 #[no_mangle] // Disabling name mangling
 pub extern "C" fn _start() -> ! { // Entry point
+    let vga_buffer = 0xb8000 as *mut u8; // Raw pointer
+
+    for (i, &byte) in HELLO.iter().enumerate() { // Iterate over the bytes of the static HELLO byte string
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte; // Using offset to write the string byte and the corresponding color byte (0xb is light cyan)
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop{}
 }
